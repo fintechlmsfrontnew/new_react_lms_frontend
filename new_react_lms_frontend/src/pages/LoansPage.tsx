f8fb04e30c2602085ff5c74ed9b8733d0d9d1653
Amisha <amishamishra057@gmail.com>
2026-02-10 14:22:33 +0530

    fix project structure

--- new_react_lms_frontend/src/pages/LoansPage.tsx
+++
@@ -1,8 +0,0 @@
-export function LoansPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold text-gray-900">Loans</h1>
-      <p className="text-gray-600 mt-2">Loans list yahan aayegi. Ye bhi Layout ke andar wrap hai.</p>
-    </div>
-  )
-}
